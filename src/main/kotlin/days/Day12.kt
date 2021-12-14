9d62ed9a022fd92821619fd0b0ad5c93f776a576
daniel.weiss <daniel.weiss@innogames.com>
2021-12-14 10:26:26 +0100

    Day 12-2

--- src/main/kotlin/days/Day12.kt
+++ src/main/kotlin/days/Day12.kt
@@ -52,6 +52,60 @@ class Day12 : Day {
         return paths.toLong()
     }
 
+    override fun executePart2(): Long {
+        val nodes = HashSet<Node>()
+        File("src/main/resources/day12-2.txt").readLines().forEach { s ->
+            val parts = s.split('-')
+            val l = parts[0]
+            val r = parts[1]
+
+            var nodeL = nodes.find { node -> node.id == l }
+            var nodeR = nodes.find { node -> node.id == r }
+
+            if (nodeL == null) {
+                nodeL = Node(l)
+            }
+
+            if (nodeR == null) {
+                nodeR = Node(r)
+            }
+
+            nodes.add(nodeL)
+            nodes.add(nodeR)
+
+            nodes.find { node -> node.id == l }!!.addNext(nodeR)
+            nodes.find { node -> node.id == r }!!.addNext(nodeL)
+        }
+
+        var paths = 0
+
+        fun traverse(node: Node, path: List<Node>) {
+            if (node.isStart() && path.any { it.id == node.id }) {
+                return
+            }
+
+            if (node.isEnd()) {
+                paths++
+
+                return
+            }
+
+            if (node.isSmall() && (path.groupingBy { it }.eachCount()
+                    .any { entry -> entry.value == 2 && entry.key.isSmall() }) && path.contains(node)
+            ) {
+                return
+            }
+
+            node.nexts.map {
+                traverse(it, path + node)
+            }
+        }
+
+        traverse(nodes.find { it.isStart() }!!, ArrayList())
+
+        return paths.toLong()
+    }
+
     data class Node(val id: String) {
         fun addNext(next: Node) {
             if (nexts.none { it.id == next.id }) {
